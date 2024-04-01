af68ed4b3613367732e71e19cdf0b198cae57674
desjardinsm <desjardinsm@MLC137A00>
2024-04-01 12:37:21 -0400

    Add project files.

---
+++ Robot/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Robot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var robot = new Robot("X001");
+            robot.MoveForward();
+            var robot2 = new Robot("X002");
+            var robot3 = new Robot("X003");
+
+            //Console.WriteLine(robot.DisplayInformation());
+            robot.Charge(5);
+            //Console.WriteLine(robot.DisplayInformation());
+
+            //Console.WriteLine(robot2.DisplayInformation());
+            //Console.WriteLine(robot3.DisplayInformation());
+
+            Console.WriteLine(robot);
+
+            Console.WriteLine(1.ToString().GetType());
+
+
+            //robot.Model = "X001";
+
+            //var robot2 = new Robot();
+
+
+            //var robot3 = new Robot() { Model = "X002" };
+        }
+    }
+}
---
+++ Robot/Robot.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robot
+{
+    public class Robot
+    {
+        /*
+        private string model;
+
+        public string GetModel()
+        {
+            return model;
+        }
+
+        public void SetModel(string model) 
+        {
+            this.model = model;
+        }
+        */
+
+        public string Model { get; init; }
+
+        public int BatteryCapacity { get; private set; }
+
+        private int _batteryLevel;
+        public int BatteryLevel 
+        {   
+            get 
+            { 
+                return _batteryLevel; 
+            }
+
+            private set 
+            { 
+                if(value < 0)
+                {
+                    _batteryLevel = 0;
+                }
+                else if (value > 100)
+                {
+                    _batteryLevel = 100;
+                }
+                else
+                {
+                    _batteryLevel = value;
+                }
+            }
+        }
+
+        private string _task;
+        public string Task 
+        { 
+            get 
+            { 
+                if(_task is null)
+                {
+                    return "No Task Set";
+                }
+                else
+                {
+                    return _task;
+                }
+            } 
+            set
+            {
+                _task = value;
+            }
+        }
+
+
+        public Robot()
+        {
+            Model= string.Empty;
+            BatteryCapacity = 1000;
+            BatteryLevel = 100;
+            Task = null;
+        }
+        public Robot(string model)
+        {
+            Model = model;
+
+            if (model == "X003")
+            {
+                Model = model;
+                BatteryCapacity = 5500;
+                BatteryLevel = 100;
+                Task = null;
+            }
+            else if(model == "X002")
+            {
+                Model = model;
+                BatteryCapacity = 2500;
+                BatteryLevel = 100;
+                Task = null;
+            }
+            else
+            {
+                Model = model;
+                BatteryCapacity = 1000;
+                BatteryLevel = 100;
+                Task = null;
+            }
+
+        }
+
+        public void Charge(int minutes)
+        {
+            Task = $"Charged: {minutes} minutes";
+
+            while(minutes-- > 0)
+            {
+                BatteryLevel++;
+            }
+
+        }
+
+        public void MoveForward()
+        {
+            //cost 1%
+            if(BatteryLevel > 1)
+            {
+                BatteryLevel--;
+                Task = "Moved Forward";
+            }
+            else
+            {
+                Task = "Unable to Move Going to Charge for 5 minutes";
+                Charge(5);
+            }
+            
+        }
+
+        public override string ToString()
+        {
+            string info = "";
+            info += $"Model: {Model}\n";
+            info += $"Batter Capacity: {BatteryCapacity}\n";
+            info += $"Battery Level: {BatteryLevel}\n";
+            info += $"Last Task: {Task}\n";
+            return info;
+        }
+      
+
+      
+    }
+}
